480f4f82cb94a37fa064395fc8cc7d984556e6b7
xiaoadong <hellogit@qq.com>
2018-08-06 19:56:57 +0800

    番茄钟第一版，可以以25,5,25,5,25,15分钟，这样循环，有暂停和重置功能

---
+++ src/cn/xiaoadong/tomatoUI/TomatoUI.java
@@ -0,0 +1,229 @@
+package cn.xiaoadong.tomatoUI;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class TomatoUI extends JFrame {
+
+	private JPanel contentPane;
+	private Boolean WR= true;
+	private Timer timer;
+	private String time;
+	private String countDown;
+	private JPanel timepanel;
+	private JLabel timeLabel;
+	private JLabel timu;
+	private JButton button;
+	private String DEFAULT_TIME_FORMAT = "yyyy-MM-dd HH:mm";
+	private int circle;//一个大循环，4番茄+3休息+1大休息
+	private volatile static Boolean WORK = false;
+	//2ge线程任务
+	timeCount tomato;
+	timeCount rest;
+	timeCount bigRest;
+	/**
+	 * Create the frame.
+	 */
+	public TomatoUI() {
+		//
+		timer = new Timer();
+		//
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		timepanel = new JPanel();
+		timepanel.setBounds(10, 47, 414, 106);
+		timepanel.setVisible(true);
+		contentPane.add(timepanel);
+		timepanel.setLayout(null);
+		
+		timeLabel = new JLabel("");
+		timeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		timeLabel.setBounds(10, 48, 394, 35);
+		timeLabel.setFont(new Font("yahei", 2, 25));
+		timeLabel.setForeground(Color.red);
+		timeLabel.setText("点击开始");
+		timepanel.add(timeLabel);
+		
+		timu = new JLabel("");
+		timu.setForeground(Color.BLUE);
+		timu.setHorizontalAlignment(SwingConstants.CENTER);
+		timu.setBounds(10, 10, 394, 28);
+		timepanel.add(timu);
+		
+		JButton button_1 = new JButton("暂停");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pause();
+			}
+
+			
+		});
+		button_1.setBounds(177, 181, 60, 30);
+		contentPane.add(button_1);
+		
+		JButton button_2 = new JButton("重置");
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				reset();
+			}
+		});
+		button_2.setBounds(247, 181, 60, 30);
+		contentPane.add(button_2);
+		
+		button = new JButton("开始");
+		
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(!WORK) {
+					WORK = true;
+					if (WR) {//启动番茄的倒计时
+						tomato = new timeCount(25, "小番茄结束");
+						button.setText("休息");
+						WR = false;
+						//番茄
+						timu.setText("番茄时间");
+						timer.schedule(tomato, new Date(), 1000);
+						circle++;
+					}
+					else {//启动休息的倒计时
+						button.setText("番茄");
+						WR = true;
+						timu.setText("休息时间");
+						if (circle == 7) {
+							//大休息
+							bigRest = new timeCount(15, "大休息结束");
+							timer.schedule(rest, new Date(), 1000);
+							circle = 0;
+						}else {
+							//小休息
+							rest = new timeCount(5, "小休息结束");
+							timer.schedule(rest, new Date(), 1000);
+							circle++;
+						}
+					}
+				}
+	}
+});
+		button.setBounds(107, 181, 60, 30);
+		contentPane.add(button);
+		
+		//显示当前时间
+		JLabel label = new JLabel();
+		label.setBounds(312, 10, 112, 30);
+		contentPane.add(label);
+		
+		timer.schedule(new TimerTask() {
+			
+			SimpleDateFormat dateFormatter = new SimpleDateFormat(DEFAULT_TIME_FORMAT);
+			@Override
+			 public void run() {
+			  time = dateFormatter.format(Calendar.getInstance().getTime());
+			  label.setText(time);
+			 }
+				
+			},
+		 new Date(), 1000);
+}
+	public void reset() {
+		if (tomato != null) {
+			tomato.resetStop();
+			tomato.cancel();
+		}
+		if (rest != null) {
+			rest.resetStop();
+			rest.cancel();
+		}
+		if (bigRest != null) {
+			bigRest.resetStop();
+			bigRest.cancel();
+		}
+		WORK = false;
+		WR = true;
+		circle = 0;
+		timeLabel.setText("点击开始");
+		timu.setText("");
+		button.setText("开始");
+		
+		
+	}
+	public void pause() {
+		if (circle%2 == 1) {
+			tomato.stop();
+		}else if (circle == 7) {
+			bigRest.stop();
+		}else {
+			rest.stop();
+		}
+	}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TomatoUI frame = new TomatoUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+	}
+//设计一个任务内部类
+	class timeCount extends TimerTask{
+		private String prompt;
+		private int fanqie;
+		private volatile boolean stop;
+		public timeCount(int n, String prompt) {
+			this.prompt = prompt;
+			fanqie = n * 60;
+			stop = false;
+		}
+
+		@Override
+		public void run() {
+			int minute = fanqie / 60;
+			int second = fanqie % 60;
+			countDown = minute + " : " + second;
+			timeLabel.setText(countDown);
+			if (fanqie <= 0) {
+				timeLabel.setText(prompt);
+				WORK = false;
+				this.cancel();
+			}
+			fanqie--;
+			while(stop) {
+			}
+		}
+		
+		public void stop() {
+			stop = !stop;
+		}
+		
+		public void resetStop() {
+			stop = false;
+		}
+	}
+}
